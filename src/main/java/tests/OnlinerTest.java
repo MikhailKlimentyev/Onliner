d74d6c730b881dba75b4bdaf47784462d7af2da8
Mikhail_Klimentsyeu <mikhail.klimentyev@gmail.com>
2021-02-23 19:50:31 +0300

    Add failed test

--- src/main/java/tests/OnlinerTest.java
+++ src/main/java/tests/OnlinerTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -64,6 +65,11 @@ public class OnlinerTest {
             .validateSearchLabels(brand, expectedFromPrice, expectedToPrice)
             .validateFoundedBrandsOnFirstPage(50, brand)
             .validateUsdPrice(fromPrice, toPrice);
+    }
+
+    @Test
+    public void test() {
+        Assert.assertTrue(false);
     }
 
     private String getNormalizedPrice(String price) {
